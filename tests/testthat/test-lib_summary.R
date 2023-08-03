efeca1699c8b5a7755d32363d6268192639425c4
MacKenzie <Will.MacKenzie@gov.bc.ca>
2023-08-03 13:52:51 -0700

    fixed tests

--- tests/testthat/test-lib_summary.R
+++ tests/testthat/test-lib_summary.R
@@ -2,9 +2,9 @@ test_that("lib_summary returns expected results", {
   expect_silent(result <- lib_summary())
   expect_equal (ncol(result),2)
   expect_s3_class (result, "data.frame")
-  expect_equal(names(result), c("library", "n_packaages"))
+  expect_equal(names(result), c("library", "n_packages"))
   expect_type(result$library, "character")
-  expect_type(result$n_packages, "double")
+  expect_type(result$n_packages, "integer")
 })
 
 test_that("lib_summary fails appropriately", {

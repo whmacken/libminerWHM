6c74f9185be0d53be47538eabba299eef9cfef3a
MacKenzie <Will.MacKenzie@gov.bc.ca>
2023-08-03 15:23:59 -0700

    change the size function with purrr function

--- R/lib_summary.R
+++ R/lib_summary.R
@@ -1,23 +1,25 @@
 #' R Library Summary
 #'
 #'Provides brief summary of installed packages on local machine
+#'
+#' @param sizes logical to show library sizes in bytes
+#'
 #' @return a data.frame containing count of packages in each library
 #' @export
 #'
 #' @examples
 #' lib_summary()
+#' lib_summary(sizes = TRUE)
 lib_summary <- function(sizes =  FALSE) {
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
   names(pkg_df) <-  c("library", "n_packages")
   if(sizes){
-    pkg_df$lib_size <- vapply(
+    pkg_df$lib_size <- map_dbl(
               pkg_df$library,
-              function(x){
-              sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
-                              },
-              FUN.VALUE = double(1))
+              ~sum(fs::file_size(fs::dir_ls(.x, recurse = TRUE))))
+
   }
   pkg_df
 }

b761e4acabe15877aeeaf150003595e10e3b7777
MacKenzie <Will.MacKenzie@gov.bc.ca>
2023-08-03 14:27:30 -0700

    add in parameter to show library sizes

--- R/lib_summary.R
+++ R/lib_summary.R
@@ -6,10 +6,18 @@
 #'
 #' @examples
 #' lib_summary()
-lib_summary <- function() {
+lib_summary <- function(sizes =  FALSE) {
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
   names(pkg_df) <-  c("library", "n_packages")
+  if(sizes){
+    pkg_df$lib_size <- vapply(
+              pkg_df$library,
+              function(x){
+              sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
+                              },
+              FUN.VALUE = double(1))
+  }
   pkg_df
 }

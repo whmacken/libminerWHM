4acffe76b2c2df29e45b2a9bbe4f97622eff57a9
MacKenzie <Will.MacKenzie@gov.bc.ca>
2023-08-03 14:26:25 -0700

    added new tests

--- tests/testthat/test-lib_summary.R
+++ tests/testthat/test-lib_summary.R
@@ -5,8 +5,16 @@ test_that("lib_summary returns expected results", {
   expect_equal(names(result), c("library", "n_packages"))
   expect_type(result$library, "character")
   expect_type(result$n_packages, "integer")
+  expect_gt(sum(result$n_packages),0)
+  expect_setequal(result$library, .libPaths())
 })
 
-# test_that("lib_summary fails appropriately", {
-#   expect_error(lib_summary("foo"), "unused arguement")
-# })
+test_that("lib_summary fails appropriately", {
+  expect_error(lib_summary("foo"), "not interpretable as logical")
+})
+test_that("size arguement works", {
+  result <- lib_summary(sizes = TRUE)
+  expect_equal(names(result),
+               c("library", "n_packages", "lib_size"))
+  expect_type(result$lib_size, "double")
+})

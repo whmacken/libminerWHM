a2e82572208ce6c9a677e607435d5004b25bb914
MacKenzie <Will.MacKenzie@gov.bc.ca>
2023-08-03 13:47:04 -0700

    added tests for lib_summary

---
+++ tests/testthat/test-lib_summary.R
@@ -0,0 +1,12 @@
+test_that("lib_summary returns expected results", {
+  expect_silent(result <- lib_summary())
+  expect_equal (ncol(result),2)
+  expect_s3_class (result, "data.frame")
+  expect_equal(names(result), c("library", "n_packaages"))
+  expect_type(result$library, "character")
+  expect_type(result$n_packages, "double")
+})
+
+test_that("lib_summary fails appropriately", {
+  expect_error(lib_summary("foo"), "unused arguement")
+})
